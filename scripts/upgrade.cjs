8d4d5c180d20a2e5a559d008bfc476538b720030
Derryl <derryl@d3labs.io>
2025-09-12 14:41:19 +0700

    add upgrade script

---
+++ scripts/upgrade.cjs
@@ -0,0 +1,54 @@
+const { ethers, upgrades } = require("hardhat");
+require('dotenv').config();
+
+async function main() {
+
+    const proxyAddress = process.env.PROXY_ADDRESS;
+    const mailboxAddress = process.env.MAILBOX_ADDRESS;
+
+    if (!proxyAddress || !mailboxAddress) {
+        throw new Error('PROXY_ADDRESS and MAILBOX_ADDRESS must be set in .env file');
+    }
+
+    const HypERC20 = await ethers.getContractFactory("HypERC20");
+
+    await upgrades.forceImport(proxyAddress, HypERC20, {
+        kind: "transparent", // or "uups" if it’s a UUPS proxy
+        constructorArgs: [18, 1n, mailboxAddress],
+        unsafeAllow: [
+            "constructor",
+            "state-variable-immutable",
+            // add this if your V2 uses reinitializer + _disableInitializers()
+            "missing-initializer",
+        ],
+    });
+
+    // (Optional but recommended) validate the storage layout before upgrading
+    const V2 = await ethers.getContractFactory("HypERC20_V2");
+    await upgrades.validateUpgrade(proxyAddress, V2, {
+        kind: "transparent",                         // or "uups" if that's your proxy
+        constructorArgs: [18, 1n, mailboxAddress],   // your impl’s constructor args (immutables)
+        unsafeAllow: [
+            "constructor",
+            "state-variable-immutable",
+            "missing-initializer",                     // if your V2 uses reinitializer + _disableInitializers()
+        ],
+    });
+
+    // Now upgrade + call your reinitializer to set the new value
+    const upgraded = await upgrades.upgradeProxy(proxyAddress, V2, {
+        kind: "transparent",
+        constructorArgs: [18, 1n, mailboxAddress],   // EXACTLY the same as V1
+        unsafeAllow: ["constructor", "state-variable-immutable", "missing-initializer",],
+        call: { fn: "initializeV2", args: [[]] },
+    });
+
+    console.log("Upgraded proxy at:", upgraded.target);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
